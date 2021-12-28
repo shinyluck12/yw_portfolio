41f422d1051721d5c00ff931f565fd71733c7f2a
shinyluck12 <shinyluck12@gmail.com>
2021-12-28 17:13:44 +0900

    금강수정,링크새로연결,스킬부분,탑버튼수정

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  $(".m1").click(function () {
+    window.open("acebed.html");
+  });
+  $(".m2").click(function () {
+    window.open("eoyeomo.html");
+  });
+  $(".m3").click(function () {
+    window.open("roundaround.html");
+  });
+  $(".m4").click(function () {
+    window.open("kumkang.html");
+  });
+  $(".m5").click(function () {
+    window.open("vuetodo.html");
+  });
+  $(".m6").click(function () {
+    window.open("acebed.html");
+  });
+
+  $(".ace_link").click(function () {
+    window.location.href = "http://vampireapire.dothome.co.kr";
+  });
+  $(".ace_link11").click(function () {
+    window.location.href = "https://github.com/shinyluck12/acebed_clone";
+  });
+  $(".ace_link2").click(function () {
+    opnNewTab.location.href = "../sub/roundaround/index.html";
+  });
+  $(".ace_link22").click(function () {
+    opnNewTab.location.href =
+      "https://github.com/shinyluck12/roundaround_clone";
+  });
+  $(".ace_link3").click(function () {
+    window.location.href = "http://vampireapire12.dothome.co.kr";
+  });
+  $(".ace_link33").click(function () {
+    window.location.href = "https://github.com/shinyluck12/eoyeomo_clone";
+  });
+  $(".ace_link4").click(function () {
+    window.location.href = "http://shinyluck93.dothome.co.kr";
+  });
+  $(".ace_link44").click(function () {
+    window.location.href = "https://github.com/shinyluck12/kumkang_renewal";
+  });
+  $(".vuetodo").click(function () {
+    window.location.href = "https://github.com/shinyluck12/vue-todo";
+  });
+  $(".s_email").click(function () {
+    window.location.href = "mailto:vampireapire@naver.com";
+  });
+  $("#top").click(function () {
+    window.location.href = "#header";
+  });
+});
