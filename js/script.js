f7b4b61622d0441f32393534bbd31e4b5db4b39d
shinyluck12 <shinyluck12@gmail.com>
2021-12-28 11:31:24 +0900

    링크연결,뷰어플추가

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  $(".m1").click(function () {
+    window.open("acebed.html");
+  });
+  $(".m2").click(function () {
+    window.open("eoyeomo.html");
+  });
+  $(".m3").click(function () {
+    window.open("roundaround.html");
+  });
+  $(".m4").click(function () {
+    window.open("kumkang.html");
+  });
+  $(".m5").click(function () {
+    window.open("vuetodo.html");
+  });
+  $(".m6").click(function () {
+    window.open("acebed.html");
+  });
+
+  $(".ace_link").click(function () {
+    window.location.href = "http://vampireapire.dothome.co.kr";
+  });
+  $(".ace_link2").click(function () {
+    opnNewTab.location.href = "../sub/roundaround/index.html";
+  });
+  $(".ace_link3").click(function () {
+    window.location.href = "http://vampireapire12.dothome.co.kr";
+  });
+  $(".ace_link4").click(function () {
+    window.location.href = "http://shinyluck93.dothome.co.kr";
+  });
+  $(".s_email").click(function () {
+    window.location.href = "mailto:vampireapire@naver.com";
+  });
+  $("#top").click(function () {
+    window.location.href = "#header";
+  });
+});
